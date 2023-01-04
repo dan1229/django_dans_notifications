40fe7da863db6a612b862935921d5f21c9119bd7
Daniel Nazarian <danielnazarian@outlook.com>
2023-01-04 16:12:06 -0500

    lint

--- django_dans_notifications/threads.py
+++ django_dans_notifications/threads.py
@@ -1,7 +1,6 @@
 from threading import Thread
 
 
-
 #
 # EmailThread
 #
@@ -10,6 +9,6 @@ class EmailThread(Thread):
     Simple email thread to send emails asynchronously.
     Simply pass a function you want to run in a thread and any arguments
     """
+
     def run(self, func, *args):
         func(*args)
-
