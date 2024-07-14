3bf1176a44a64495df5396d8d2b311dc57d2808c
Daniel Nazarian <danielnazarian@outlook.com>
2024-07-13 20:36:39 -0400

    str_to_bool tsts

---
+++ django_dans_notifications/test/test_helpers.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from ..helpers import str_to_bool
+
+
+class TestStrToBool(TestCase):
+
+    def test_true_values(self):
+        true_values = ["yes", "true", "t", "1", "on", "YES", "True", "T", "1", "ON"]
+        for value in true_values:
+            with self.subTest(value=value):
+                self.assertTrue(str_to_bool(value))
+
+    def test_false_values(self):
+        false_values = [
+            "no",
+            "false",
+            "f",
+            "0",
+            "off",
+            "NO",
+            "False",
+            "F",
+            "0",
+            "OFF",
+            "",
+            None,
+            "random",
+            123,
+        ]
+        for value in false_values:
+            with self.subTest(value=value):
+                self.assertFalse(str_to_bool(value))
